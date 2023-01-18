ec539f5664c664f9fa6039238a97b84f6a24c4cd
Aoxiang Zhang <ax706429@dal.ca>
2023-01-18 08:44:58 -0500

    added test cases, test coverage is 100% now

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { UsersComponent } from './users.component';
 
@@ -19,5 +21,27 @@ describe('UsersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render user id', () => {
+    const fixture = TestBed.createComponent(UsersComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('span.badge')?.textContent).toContain(1);
+  });
+
+  it('should render user name', () => {
+    const fixture = TestBed.createComponent(UsersComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('span.name')?.textContent).toContain('User-1');
+  });
+
+  // ngIf related
+  it('Should show description after clicking button', () => {
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    button.click();
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#user-description')).nativeElement).toBeTruthy();
   });
 });
