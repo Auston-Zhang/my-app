c4a65e36570c415aefaded5dc5cd15e98f4ad24e
Aoxiang Zhang <ax706429@dal.ca>
2023-01-18 18:48:46 -0500

    updated css to follow responsive design

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,7 +32,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Test-Auston app is running!');
+    expect(compiled.querySelector('.content h3')?.textContent).toContain('Test-Auston');
   });
 
   
